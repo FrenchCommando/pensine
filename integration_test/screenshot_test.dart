0f84ab33cac40aa4018ea55a5044503b509439eb
FrenchCommando <martialren@gmail.com>
2026-04-16 09:14:51 -0400

    fix imports

--- integration_test/screenshot_test.dart
+++ integration_test/screenshot_test.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
