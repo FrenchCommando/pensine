136daa2469242a2a40e13d9b8896ce9bb176f9f0
FrenchCommando <martialren@gmail.com>
2026-04-16 16:30:00 -0400

    write on failure

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:integration_test/integration_test_driver.dart';
 
 Future<void> main() => integrationDriver(
+      writeResponseOnFailure: true,
       responseDataCallback: (data) async {
         if (data == null) return;
         final screenshots = data['screenshots'] as List<dynamic>?;
